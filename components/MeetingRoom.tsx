12b6a6980094f5d52a65e85ff3b3eb95609cac6c
huywb <pthhuy1306@gmail.com>
2025-10-16 17:52:46 +0700

    MeetingRoom components

---
+++ components/MeetingRoom.tsx
@@ -0,0 +1,68 @@
+import { CallControls, CallParticipantsList, CallStatsButton, PaginatedGridLayout, SpeakerLayout } from '@stream-io/video-react-sdk'
+import React, { useState } from 'react'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { LayoutList } from 'lucide-react'
+
+type CallLayoutType = 'grid' | 'speaker-left' | 'speaker-right'
+
+const MeetingRoom = () => {
+    const [layout,setLayout] = useState<CallLayoutType>('speaker-left')
+    const [showParticipants,setShowParticipants] = useState(false)
+
+    const CallLayout = ()=>{
+        switch (layout) {
+            case 'grid':
+                return <PaginatedGridLayout></PaginatedGridLayout> 
+            case 'speaker-left':
+                return <SpeakerLayout participantsBarPosition='left'></SpeakerLayout>
+            default:
+                return <SpeakerLayout participantsBarPosition='right'></SpeakerLayout>
+
+        }
+    }
+
+  return (
+    <section className='relative h-screen w-full overflow-hidden pt-4 text-white'>
+      <div className='relative flex size-full items-center justify-center'>
+        <div className='flex size-full max-w-[1000px] items-center'>
+            <CallLayout></CallLayout>
+        </div>
+        <div className={`${showParticipants ? 'show-block' : ''}h-[calc(100vh-86px)] hidden ml-2`}>
+            <CallParticipantsList onClose={()=>setShowParticipants(false)}></CallParticipantsList>
+        </div>
+      </div>
+
+      <div className='fixed bottom-0 flex w-full items-center justify-center gap-5'>
+        <CallControls></CallControls>
+
+        <DropdownMenu>
+            <div className='flex items-center'>
+            <DropdownMenuTrigger className='cursor-pointer rounded-2xl bg-[#19232d px-4 py-2 hover:bg-[#4c535b'>
+                <LayoutList size={20} className='text-white'></LayoutList>
+            </DropdownMenuTrigger>
+            </div>
+            <DropdownMenuContent className='border-dark-1 bg-dark-1 text-white'>
+                {['Grid','Speaker-Left','Speaker-Right'].map((item,index)=>(
+                    <div key={index}>
+                        <DropdownMenuItem onClick={()=>{setLayout(item.toLowerCase() as CallLayoutType)}} className='cursor-pointer'>
+                            {item}
+                        </DropdownMenuItem>
+                        <DropdownMenuSeparator className='border-dark-1' />
+                    </div>
+                ))}
+            </DropdownMenuContent>
+        </DropdownMenu>
+        <CallStatsButton></CallStatsButton>
+      </div>
+    </section>
+  )
+}
+
+export default MeetingRoom
