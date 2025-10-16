29d164aec1c3ffe4cd25613b6c0b62f2b13decd7
huywb <pthhuy1306@gmail.com>
2025-10-16 17:50:51 +0700

    SignIn SignUp page

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/sonner"
+import "@stream-io/video-react-sdk/dist/css/styles.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -41,6 +43,7 @@ export default function RootLayout({
             colorInputBackground: '#252a41',
             colorInputText:'#fff'}}}>
         {children}
+         <Toaster position="top-center" />
       </ClerkProvider>
       </body>
     </html>
