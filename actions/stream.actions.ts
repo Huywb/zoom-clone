4b1f86df7cff9eb1bbd70944a50ad6e8ea50176b
huywb <pthhuy1306@gmail.com>
2025-10-17 14:19:13 +0700

    Setup middleware and Stream actions

---
+++ actions/stream.actions.ts
@@ -0,0 +1,27 @@
+'use server'
+
+import { currentUser } from "@clerk/nextjs/server";
+import { StreamClient } from "@stream-io/node-sdk";
+import { StreamVideoClient } from "@stream-io/video-react-sdk";
+
+
+const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
+const apiSecret = process.env.STREAM_SECRET_KEY
+
+export const tokenProvider = async()=>{
+    const user = await currentUser()
+    if(!user) throw Error("User not login")
+    if(!apiKey) throw Error("No API key")
+    if(!apiSecret) throw Error("No API secret")
+
+    const client = new StreamClient(apiKey,apiSecret)
+    const client2 = new StreamVideoClient(apiKey)
+
+    const exp = Math.round(new Date().getTime()/ 1000) + 60 * 60
+
+    const issued = Math.floor(Date.now()/1000) - 60
+
+    const token = client.createToken(user.id,exp, issued)
+
+    return token
+}
